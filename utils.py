c58bc79d66d15f99edc9d9f1cfd988e79dfffcf8
jopaulnelson <jopaulnelsonw105@gmail.com>
2026-05-16 14:08:48 +0530

    Fix save_model function parameter syntax

--- utils.py
+++ utils.py
@@ -22,8 +22,8 @@ def perform_clustering(df):
     model, labels, pca_result = perform_clustering(df)
     return model, labels, pca_result
 
-def save_model(clustering_model,'buyer_segmentation_model.pkl')
-    joblib.dump(clustering_model,'buyer_segmentation_model.pkl')
+def save_model(clustering_model,buyer_segmentation_model.pkl)
+    joblib.dump(clustering_model,buyer_segmentation_model.pkl)
 
 def load_model('buyer_segmentation_model.pkl')
     return joblib.load('buyer_segmentation_model.pkl')
